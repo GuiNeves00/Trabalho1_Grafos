d51f595e99892251674a6b234b22b52ff1cea4a5
Guilherme <guiinevessilva@gmail.com>
2022-05-18 09:26:47 -0300

    tds alg. feitos

--- tp1_grafo.py
+++ tp1_grafo.py
@@ -1,3 +1,8 @@
+from math import dist
+from re import X
+from xml.etree.ElementPath import prepare_descendant
+
+
 class Grafo:
 
   def __init__(self, num_vert = 0, num_arestas = 0, lista_adj = None, mat_adj = None):
@@ -112,7 +117,6 @@ class Grafo:
           R.append(v)
           desc[v] = 1
     return R
-
   def busca_profundidade(self, s):
     """Retorna a ordem de descoberta dos vertices pela 
        busca em profundidade a partir de s"""
@@ -194,27 +198,96 @@ class Grafo:
 
     #Q.pop(u) ou Q[u] = True | MELHORIA ALGORITMO: o vert. origem sempre sera o primeiro a ser processado
     while True: #L6
-      if Q and all(elem == True for elem in Q):
-        break
+      ctrl = False
+      #if Q and all(elem == True for elem in Q):
+        #break
       menor_dist = float('inf') #menor_dist inicia com infinito porque as distancias ainda sao desconhecidas, exceto a origem. Assim tornando possível a busca pelo menor valor do vetor dist, 
                                 #pois, se o valor de menor_dist for >= que o do indice atual, logo o indice atual sera menor e então sera atribuido o valor, encontrando o "novo menor".
       for i in range(len(Q)): #percorre o vetor Q através de seu tamanho, quer será reduzido (Q.pop) na linha 203.
         if menor_dist >= dist[i] and Q[i] != True: #MELHORIA ALGORITMO: L193
           menor_dist = dist[i]
           u = i #u recebe o indice que representa o vert. de menor distancia no vetor dist.
+          ctrl = True
+        elif i == len(Q)-1 and ctrl == False:
+          return dist, pred
+          
       Q[u] = True #Equivalente a Q.pop(u)
       
       #fazer depois: "se dado grafo tiver mtts vertices, usar mat. caso contrario, usar lista"
+      #"Mais uma dica (caso queira ainda melhorar). Se implementar com lista de adjacências fica mais rápido. Tem alguns grafos também que não dá para carregar com matriz de adjacências pela limitação de memória."
       for v in range(self.num_vert): #dentro desse laço, percorremos o grafo em formato de matriz de adj (num_vert = total de repeticoes necessarias, pq percorremos toda a matriz).
         #u representa a linha da matriz que eh o vert. de menor distancia do vetor dist. isso faz com que percorremos apenas o "necessario".
         if self.mat_adj[u][v] != 0 and dist[v] > dist[u] + self.mat_adj[u][v]:
           dist[v] = dist[u] + self.mat_adj[u][v]
           pred[v] = u
     
-    return dist, pred
+    #return dist, pred
 
     #LOGICA DA LINHA 6 ANTERIORMENTE:
       #for i in range(len(dist)): #L7: i percorre dist 
         #if Q[i] != None and dist[i] < dist[u]: #L7: checa se o vértice i já foi computado e verifica o menor valor de dist
           #u = dist[i] #L7: u recebe o menor valor de dist
       #Q.pop(u) #L8: remove u de Q
+  
+  def bellman_ford (self, s):
+    dist = [float('inf') for v in range (self.num_vert)]
+    pred = [None for v in range (self.num_vert)]
+    dist[s] = 0
+    E = [(None, None, None) for x in range(self.num_arestas)] #lista de tuplas contendo arestas e seu peso
+    x = 0
+
+    #obtem os valores da lista E
+    for i in range(len(self.mat_adj)):
+      for j in range(len(self.mat_adj[i])):
+        if self.mat_adj[i][j] != 0:
+          E[x] = (i, j, self.mat_adj[i][j])
+          x = x + 1
+
+    #Laco principal
+    for i in range(self.num_vert - 1):
+      trocou = False
+      for (u, v, w) in E:
+        if dist[v] > dist[u] + w:
+          dist[v] = dist[u] + w
+          pred[v] = u
+          trocou = True
+
+      if trocou == False:
+        return dist, pred
+
+  def BuscaLarga(self, s):
+    dist = [float('inf') for v in range(self.num_vert)] #Inicializa a função com todas as distancias do  vertice vert recebendo infinito
+    pred = [None for v in range(self.num_vert)] #Predecessores no vertice vert recebem null pois não se sabe o caminho minimo
+    dist[s] = 0   #A distancia da origem para ela mesma vai ser 0
+
+    Q = [s] #A lista q recebe o vetor de origem s
+
+
+    while len(Q) != 0: #Enquanto o tamanho da lista q for diferente de 0
+      u = Q.pop(0) #Remove o primeiro elemento de q e adciona ele em u
+      for (v, w) in self.lista_adj[u]: #para cada vert na lista de adj de u faz:
+        if dist[v] == float('inf'): #se a distancia do vert = inf significa q não foi percorrido o no,dessa forma ira percorrer
+          Q.append(v) #Adciona o vert no final da lista q
+          dist[v] = dist[u] + 1 #Se atualiza a distancia do vert passando por u+1
+          pred[v] = u #O predecessor do vert é atualizado recebendo o valor de u
+    return dist, pred #Retorna a distancia e os predecessores
+  
+  def busca_largura_alterado(self, s):
+    """Retorna a ordem de descoberta dos vertices pela 
+    busca em largura a partir de s"""
+    #inicio = time.time() # Inicio do Timer para calcular o tempo de execução
+    
+    pred = [None for v in range(self.num_vert)]
+    dist = [float('inf') for v in range(self.num_vert)]
+    dist[s]=0
+    Q = [s]
+  
+    while Q:
+      u = Q.pop(0)
+      for (v, w) in self.lista_adj[u]:
+        if dist[v] == float("inf"):
+          dist[v] = dist[u]+1
+          pred[v]=u
+          Q.append(v)
+  
+    return dist, pred
--- tp1_main.py
+++ tp1_main.py
@@ -1,7 +1,79 @@
+from pickle import TRUE
 import sys
 sys.path.append("..")
 import tp1_grafo as grafoT1
+import time
+
+def eh_ponderado(Grafo):
+    ponderado = False
+    for i in range(len(Grafo.lista_adj)):
+        for j in range(len(Grafo.lista_adj[i])):
+            if Grafo.lista_adj[i][j][1] != 1:
+                return False
+            elif Grafo.lista_adj[i][j][1] == 1:
+                ponderado = True
+    return ponderado
+
+def aresta_negativa(Grafo):
+    for i in range(len(Grafo.lista_adj)):
+        for j in range(len(Grafo.lista_adj[i])):
+            if Grafo.lista_adj[i][j][1] < 0:
+                return True
+    
+    return False
+
+def obtem_caminho(s, t, pred):
+    caminho = [t]
+    x = t
+
+    while x != s: # Linhas para recuperar o caminho de um vertice ao outro.
+        x = pred[x]
+        caminho.append(x)
+    
+    caminho.reverse() # Linha para inverter o conteudo de "caminho" para recuperar o caminho corretamente.
+
+    return caminho
+
+def resultados(origem, destino, caminho, custo):
+    print("Origem: ", origem)
+    print("Destino: ", destino)
+    print("Caminho: ", caminho)
+    print("Custo: ", custo)
 
 grafo = grafoT1.Grafo()
-grafo.ler_arquivo("toy.txt")
-print(grafo.dijkstra(0))
+nome_arq = input("Digite o nome do arquivo:")
+grafo.ler_arquivo(nome_arq)
+
+
+if eh_ponderado(grafo) == True:
+    print("p")
+    origem = int(input("Digite a ORIGEM: "))
+    destino = int(input("Digite o DESTINO: "))
+    caminho = obtem_caminho(origem, destino, (grafo.BuscaLarga(origem))[1])
+    custo = grafo.BuscaLarga(origem)[0][destino]
+    resultados(origem, destino, caminho, custo)
+elif aresta_negativa(grafo) == True:
+    print("n")
+    origem = int(input("Digite a ORIGEM: "))
+    destino = int(input("Digite o DESTINO: "))
+    caminho = obtem_caminho(origem, destino, (grafo.bellman_ford(origem))[1])
+    custo = grafo.bellman_ford(origem)[0][destino]
+    resultados(origem, destino, caminho, custo)
+else:
+    print("d")
+    origem = int(input("Digite a ORIGEM: "))
+    destino = int(input("Digite o DESTINO: "))
+    caminho = obtem_caminho(origem, destino, (grafo.dijkstra(origem))[1])
+    custo = grafo.dijkstra(origem)[0][destino]
+    resultados(origem, destino, caminho, custo)
+
+
+
+
+#start_time = time.time()
+#print("Tempo de execucao: ", (time.time() - start_time))
+
+#print("BELLMAN-FORD:", grafo.bellman_ford(0))
+#print("DIJKSTRA:", grafo.dijkstra(0))
+#print("LARGURA (DV):", grafo.busca_largura_alterado(0))
+#print("LARGURA (GK):", grafo.BuscaLarga(0))
